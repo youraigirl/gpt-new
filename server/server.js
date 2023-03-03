49ebbc9f7a23f97dfb795c49bada0ea38fc94638
becomealpha <120771676+becomealpha@users.noreply.github.com>
2023-03-03 12:08:10 -0500

    first

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.post('/', async (req, res) => {
         const response = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [{"role": "user", "content": `${prompt}`}],
-            temperature: 0.5, // Higher values means the model will take more risks.
+            temperature: 0.5,
             max_tokens: 200, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
             top_p: 1, // alternative to sampling with temperature, called nucleus sampling
             frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
